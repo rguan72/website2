6372050f07c973ca8398138212533fd566c06090
guanr <guanr@umich.edu>
2019-01-17 19:08:24 -0500

    Messy commit to save work

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 This is my personal website, created with [React](https://reactjs.org/).
 
+Scrolling animations were done with an [Animate on Scroll](https://github.com/michalsnik/aos) library.
+
 CSS was done with [styled-components](https://www.styled-components.com/).
 
-The [wireframe](https://wireframe.cc/BJAPTp) was made with [wireframe.cc](https://wireframe.cc/).
+The [wireframe](https://wireframe.cc/HGxRF6) was made with [wireframe.cc](https://wireframe.cc/).
--- src/App.js
+++ src/App.js
@@ -1,69 +1,33 @@
 import React, { Component } from 'react';
-import styled from 'styled-components';
-import { generator } from 'uigradients';
-import logo from './logo.svg';
-import mpowered from './img/mpowered.png';
-import solarcar from './img/solarcar.png';
-import github from './img/github.png';
-import linkedin from './img/linkedin.png';
+import Fade from 'react-reveal/Zoom'
+// import logo from './logo.svg';
+// import filler from './img/Filler.png'
+// import shs from './img/startup-highschool-navbar.png';
+// import solarcar from './img/solarcar.png';
+// import github from './img/github.png';
+// import linkedin from './img/linkedin.png';
+// import richard from './img/Richard.gif';
 import './App.css';
 
-/////// CSS ///////////////
-const Background = styled.div`
-  ${generator({gradient: 'starfall'})}
-  padding: 10em;
-  height: 5%;
-  display: block;
-`
-const Name = styled.div`
-  font-family: 'Montserrat', sans-serif;
-  font-size: 10em;
-  color: white;
-  text-align: center;
-`;
 
-const Description = styled.div`
-  font-family: 'Montserrat', sans-serif;
-  font-size: 4 em;
-  color: white;
-  text-align: center;
-`;
-
-const SolarCarImg = styled.img`
-  width: 46%;
-  float: left;
-  margin-right: 10px;
-`
-///////////////////////
-
-//////////////////// REACT COMPONENTS BEGIN /////////////////
-
-class Project1 extends Component {
+class Top extends Component {
   render() {
     return (
-      <div>
-        filler
+      <div className="grad-1">
+        <h1 className="name">Richard Guan</h1>
+        <h2 className="description"> Student, entrepreneur, and math/cs lover at the University of Michigan </h2>
+        <img src={richard} alt="Richard" className="richard-img"></img>
       </div>
     );
   }
 }
 
-
 class App extends Component {
   render() {
     return (
       <div>
-        <Background>
-            <Name>Richard Guan</Name>
-            <Description> Student, entrepreneur, and math/cs lover at the University of Michigan </Description>
-            <a href='https://www.linkedin.com/in/richard-g-403534124/'>
-              <img src={linkedin} className='social-img' align="left" alt='LinkedIn'></img>
-            </a>
-            <a href='https://github.com/rguan72'>
-              <img src={github} className='social-img' align="right" alt='GitHub'></img>
-            </a>
-        </Background>
-            <Project1 />
+        <Top/>
+
       </div>
     );
   }
